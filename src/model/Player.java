58b802072c7da7164e16054e0a7ed0d45cf25c1c
WU Kejia <kejia.wu@etu.unice.fr>
2018-10-06 06:28:58 +0800

    ajouter le compteur et simplify les funtions

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,130 +13,138 @@ import java.util.Scanner;
  * @University: Polytech Nice-Sophia
  */
 public class Player {
-    String name;
-    List<Card> handcard = new ArrayList<>();
-    Player player1;
-    Player player2;
-    Scanner in = new Scanner(System.in);
-    private ArrayList<Card> listePair = new ArrayList<>();
+	String name;
+	List<Card> handcard = new ArrayList<>();
+	Player player1;
+	Player player2;
+	Scanner in = new Scanner(System.in);
+	private List<Integer> compteur = new ArrayList<Integer>();
 
-    public Player(String name) {
-        this.name = name;
-        this.handcard = new ArrayList<>();
-    }
+	public Player(String name) {
+		this.name = name;
+		this.handcard = new ArrayList<>();
+	}
 
-    public void addCard(Card card) {
-        this.handcard.add(card);
-    }
+	public void addCard(Card card) {
+		this.handcard.add(card);
+	}
 
-    public void showAllCard() {
-        for (Card card : handcard) {
-            System.out.println(card);
-        }
-    }
+	public void showAllCard() {
+		for (Card card : handcard) {
+			System.out.println(card);
+		}
+	}
 
-//    public int getNumberofCard() {
-//        return handcard.size();
-//    }
+	public int getNumberofCard() {
+		return handcard.size();
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public List<Card> getHandcard() {
-        return handcard;
-    }
+	public List<Card> getHandcard() {
+		return handcard;
+	}
 
-//    public void show() {
-//        System.out.println("Name: " + player1.getName());
-//        player1.showAllCard();
-//        System.out.println(" ");
-//        System.out.println("Name: " + player2.getName());
-//        player2.showAllCard();
-//        System.out.println(" ");
-//    }
+	public void show() {
+		System.out.println("Name: " + player1.getName());
+		player1.showAllCard();
+		System.out.println(" ");
+		System.out.println("Name: " + player2.getName());
+		player2.showAllCard();
+		System.out.println(" ");
+	}
 
-    public void playerOneAddCard() {
-        int sc = in.nextInt();
-        if(sc == 1) {
-            sc += 13;
-        }
-        Card c1 = new Card(Rank.getRank(sc));
-        player1.addCard(c1);
-    }
+	public void playerOneAddCard() {
+		int sc = in.nextInt();
+		if (sc == 1) {
+			sc += 13;
+		}
+		Card c1 = new Card(Rank.getRank(sc));
+		player1.addCard(c1);
+	}
 
-    public void playerTwoAddCard() {
-        int sc1 = in.nextInt();
-        if(sc1 == 1) {
-            sc1 += 13;
-        }
-        Card c2 = new Card(Rank.getRank(sc1));
-        player2.addCard(c2);
-    }
-    /*
-     * pour obtenir la carte la plus grande dans une main
-     */
-    public Card max(List<Card> handcard) {
-        Card cardMax = handcard.get(0);
-        for (int i = 0; i < handcard.size(); i++) {
-            if (cardMax.getValue() <= handcard.get(i).getValue()) {
-                cardMax = handcard.get(i);
-            }
-        }
-        return cardMax;
-    }
+	public void playerTwoAddCard() {
+		int sc1 = in.nextInt();
+		if (sc1 == 1) {
+			sc1 += 13;
+		}
+		Card c2 = new Card(Rank.getRank(sc1));
+		player2.addCard(c2);
+	}
 
+	
+	public Card max(List<Card> handcard) {
+		Card cardMax = handcard.get(0);
+		for (int i = 0; i < handcard.size(); i++) {
+			if (cardMax.getValue() <= handcard.get(i).getValue()) {
+				cardMax = handcard.get(i);
+			}
+		}
+		return cardMax;
+	}
 
-    /*
-     * pour tester est ce qu'il y a une pair
-     */
-    public boolean havePair(List<Card> handCard1) {
-        for (int i = 0; i < handCard1.size(); i++) {
-            for (int j = i + 1; j < handCard1.size(); j++) {
-                if (handCard1.get(i).getValue() == handCard1.get(j).getValue()) {
-                    listePair.add(handCard1.get(i));
-//                    return handCard1.get(i).getValue();
-                }
-            }
-        }
-        if (listePair.size() == 1) {
-            return true;
-        } else{
-            return false;
-        }
-    }
+	
+	public int Compteur(List<Card> handCard1) {
+		for (int i = 0; i < handCard1.size(); i++) {
+			for (int j = i + 1; j < handCard1.size(); j++) {
+				if (handCard1.get(i).getValue() == handCard1.get(j).getValue()) {
+					compteur.add(handCard1.get(i).getValue());
 
-    /*
-     * Pour tester la présence d'un brelan
-     */
+				}
+			}
+		}
+		return compteur.size();
+	}
 
-    public int haveBrelan(List<Card> handCard1) {
-        for (int i = 0; i < handCard1.size(); i++) {
-            for (int j = i + 1; j < handCard1.size(); j++) {
-                if (handCard1.get(i).getValue() == handCard1.get(j).getValue()) {
-                    for (int k = j + 1; k < handCard1.size(); k++) {
-                        if (handCard1.get(k).getValue() == handCard1.get(j).getValue()) {
-                            return handCard1.get(i).getValue();
-                        }
-                    }
+	public boolean havePair(List<Card> handCard1) {
+		Compteur(handCard1);
+		if (compteur.size() == 1) {
+			System.out.println(compteur.size());
+			return true;
+		}
+		return false;
+	}
 
-                }
-            }
-        }
-        return 0;
-    }
+	public boolean haveDoublePair(List<Card> handCard1) {
+		Compteur(handCard1);
+		if (compteur.size() == 2) {
+			System.out.println(compteur.size());
+			return true;
+		}
+		return false;
+	}
 
+	public boolean haveBrelan(List<Card> handCard1) {
+		Compteur(handCard1);
+		if (compteur.size() == 3) {
+			System.out.println(compteur.size());
+			return true;
+		}
+		return false;
+	}
 
-//    public List<Card> listePair(Card carte){
-//        listePair.add(carte);
-//        return listePair;
-//    }
+	public boolean haveFull(List<Card> handCard1) {
+		Compteur(handCard1);
+		if (compteur.size() == 4) {
+			System.out.println(compteur.size());
+			return true;
+		}
+		return false;
+	}
 
-    public boolean haveDoublePair(List<Card> handCard1) {
-        if (listePair.size()==2) {
-            System.out.println(listePair.size());
-            return true;
-        }
-        return false;
-    }
+	public boolean haveCarre(List<Card> handCard1) {
+		Compteur(handCard1);
+		if (compteur.size() == 6) {
+			System.out.println(compteur.size());
+			return true;
+		}
+		return false;
+
+	}
+	
+	public void paixu(List<Integer> handCard) {
+		Collections.sort(handCard);
+	}
 }
