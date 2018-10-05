b95085052313bf7639b7f963d3016ba21d6b5d7c
Douchin-Julien <43064086+Douchin-Julien@users.noreply.github.com>
2018-10-05 14:36:06 +0200

    ajouts de nouveaux tests pour l'égalité des cartes mais il faut encore corriger ceux sur la différence

--- test/test/test.java
+++ test/test/test.java
@@ -6,6 +6,7 @@ import model.Comparison;
 import model.Player;
 import model.Rank;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -67,26 +68,70 @@ class test {
           cardlist2.add(card6);
           assertTrue(!comparison.equals(cardlist1, cardlist2));
       }
-  //
-//      @org.junit.jupiter.api.Test
-//      void testEquals2() {
-//          Card card1 = new Card(Rank.EIGHT);
-//          Card card2 = new Card(Rank.FIVE);
-//          Card card3 = new Card(Rank.TWO);
-//          Card card4 = new Card(Rank.TWO);
-//          Card card5 = new Card(Rank.EIGHT);
-//          Card card6 = new Card(Rank.FIVE);
-//          List<Card> cardlist1 = new ArrayList<Card>();
-//          List<Card> cardlist2 = new ArrayList<Card>();
-//          cardlist1.add(card1);
-//          cardlist1.add(card2);
-//          cardlist1.add(card3);
-//          cardlist2.add(card4);
-//          cardlist2.add(card5);
-//          cardlist2.add(card6);
-//          assertTrue(!comparison.equals(cardlist1, cardlist2));
-//      }
-  //
+  
+      @org.junit.jupiter.api.Test
+      void testEqualsTroisCartesMainsIdentiques() {
+          Card card1 = new Card(Rank.EIGHT);
+          Card card2 = new Card(Rank.FIVE);
+          Card card3 = new Card(Rank.TWO);
+          Card card4 = new Card(Rank.TWO);
+          Card card5 = new Card(Rank.EIGHT);
+          Card card6 = new Card(Rank.FIVE);
+          List<Card> cardlist1 = new ArrayList<Card>();
+          List<Card> cardlist2 = new ArrayList<Card>();
+          cardlist1.add(card1);
+          cardlist1.add(card2);
+          cardlist1.add(card3);
+          cardlist2.add(card4);
+          cardlist2.add(card5);
+          cardlist2.add(card6);
+          assertTrue(!comparison.equals(cardlist1, cardlist2));
+      }
+      @org.junit.jupiter.api.Test
+      void testEqualsQuatreCartesMainsIdentiques() {
+          Card card1 = new Card(Rank.EIGHT);
+          Card card2 = new Card(Rank.FIVE);
+          Card card3 = new Card(Rank.TWO);
+          Card card4 = new Card(Rank.ACE);
+          Card card5 = new Card(Rank.TWO);
+          Card card6 = new Card(Rank.EIGHT);
+          Card card7 = new Card(Rank.FIVE);
+          Card card8 = new Card(Rank.ACE);
+          List<Card> cardlist1 = new ArrayList<Card>();
+          List<Card> cardlist2 = new ArrayList<Card>();
+          cardlist1.add(card1);
+          cardlist1.add(card2);
+          cardlist1.add(card3);
+          cardlist1.add(card4);
+          cardlist2.add(card5);
+          cardlist2.add(card6);
+          cardlist2.add(card7);
+          cardlist1.add(card8);
+          assertTrue(!comparison.equals(cardlist1, cardlist2));
+      }
+      @org.junit.jupiter.api.Test
+      void testEqualsQuatreCartesMainsDifférentes() {
+          Card card1 = new Card(Rank.EIGHT);
+          Card card2 = new Card(Rank.FIVE);
+          Card card3 = new Card(Rank.TWO);
+          Card card4 = new Card(Rank.JACK);
+          Card card5 = new Card(Rank.QUEEN);
+          Card card6 = new Card(Rank.TWO);
+          Card card7 = new Card(Rank.THREE);
+          Card card8 = new Card(Rank.ACE);
+          List<Card> cardlist1 = new ArrayList<Card>();
+          List<Card> cardlist2 = new ArrayList<Card>();
+          cardlist1.add(card1);
+          cardlist1.add(card2);
+          cardlist1.add(card3);
+          cardlist1.add(card4);
+          cardlist2.add(card5);
+          cardlist2.add(card6);
+          cardlist2.add(card7);
+          cardlist1.add(card8);
+          assertTrue(!comparison.equals(cardlist1, cardlist2));
+      }
+  
 //      @org.junit.jupiter.api.Test
 //      void testCompareHighCard() {
 //          Card card1 = new Card(Rank.TWO);
