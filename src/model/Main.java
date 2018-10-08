f4fdfeb8981c51efc8c496c902eb32cd02fba72d
Douchin-Julien <43064086+Douchin-Julien@users.noreply.github.com>
2018-10-08 20:43:09 +0200

    simplification de la classe Main

--- src/model/Main.java
+++ src/model/Main.java
@@ -16,30 +16,18 @@ public class Main {
         Comparison comparison = new Comparison();
 
         System.out.print("Main 1: ");
-
-        String r1 = in.next();
-        comparison.player1.addCard(new Card(Rank.getRank(r1.substring(0,r1.length()-2)), Color.getColor(r1.substring(r1.length() - 2))));
-        String r2 = in.next();
-        comparison.player1.addCard(new Card(Rank.getRank(r2.substring(0,r2.length()-2)),Color.getColor(r2.substring(r2.length() - 2))));
-        String r3 = in.next();
-        comparison.player1.addCard(new Card(Rank.getRank(r3.substring(0,r3.length()-2)),Color.getColor(r3.substring(r3.length() - 2))));
-        String r4 = in.next();
-        comparison.player1.addCard(new Card(Rank.getRank(r4.substring(0,r4.length()-2)),Color.getColor(r4.substring(r4.length() - 2))));
-        String r5 = in.next();
-        comparison.player1.addCard(new Card(Rank.getRank(r5.substring(0,r5.length()-2)),Color.getColor(r5.substring(r5.length() - 2))));
-
+        
+        for ( int i=0; i<5;i++) {
+        	String ri = in.next();
+        	comparison.player1.addCard(new Card(Rank.getRank(ri.substring(0,ri.length()-2)), Color.getColor(ri.substring(ri.length() - 2))));
+        }
         System.out.print("Main 2: ");
+        
+        for ( int i=5; i<10;i++) {
+        	String ri = in.next();
+        	comparison.player2.addCard(new Card(Rank.getRank(ri.substring(0,ri.length()-2)), Color.getColor(ri.substring(ri.length() - 2))));
+        }
 
-        String r6 = in.next();
-        comparison.player2.addCard(new Card(Rank.getRank(r6.substring(0,r6.length()-2)), Color.getColor(r6.substring(r6.length() - 2))));
-        String r7 = in.next();
-        comparison.player2.addCard(new Card(Rank.getRank(r7.substring(0,r7.length()-2)),Color.getColor(r7.substring(r7.length() - 2))));
-        String r8 = in.next();
-        comparison.player2.addCard(new Card(Rank.getRank(r8.substring(0,r8.length()-2)),Color.getColor(r8.substring(r8.length() - 2))));
-        String r9 = in.next();
-        comparison.player2.addCard(new Card(Rank.getRank(r9.substring(0,r9.length()-2)),Color.getColor(r9.substring(r9.length() - 2))));
-        String r10 = in.next();
-        comparison.player2.addCard(new Card(Rank.getRank(r10.substring(0,r10.length()-2)),Color.getColor(r10.substring(r10.length() - 2))));
 
         in.close();
         System.out.println(comparison.player1.handcard);
